ea3672f62fdeea5d7e4d0b9352f9d83e4469c7d7
Michael Osburn <michael.osburn@mheducation.com>
2017-02-14 08:51:31 -0800

    adding account tags def

--- turbotutils/account.py
+++ turbotutils/account.py
@@ -100,3 +100,39 @@ def create_user_access_keys(turbot_api_access_key, turbot_api_secret_key, turbot
     akey = responseObj['accessKeyId']
     skey = responseObj['secretAccessKey']
     return (akey, skey)
+
+
+def get_account_tags(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host, turbot_account):
+    """ Sets user access AKIA key pairs for a specified account
+
+    NOTE: This requires a Cluster role Turbot/Owner or higher in order to work.
+    """
+    api_method = "GET"
+    api_url = "/api/v1/accounts/%s/" % (turbot_account)
+    response = requests.request(
+        api_method,
+        urllib.parse.urljoin(turbot_host, api_url),
+        auth=(turbot_api_access_key, turbot_api_secret_key),
+        verify=turbot_host_certificate_verification,
+        headers={
+            'content-type': "application/json",
+            'cache-control': "no-cache"
+        }
+    )
+
+    responseObj = json.loads(response.text)
+    print(responseObj)
+
+if __name__ == '__main__':
+    import turbotutils
+    # Set to False if you do not have a valid certificate for your Turbot Host
+    turbot_host_certificate_verification = True
+
+    # Set to your Turbot Host URL
+    turbot_host = turbotutils.get_turbot_host()
+    turbot_account = 'aaz'
+    # Get the access and secret key pairs
+    (turbot_api_access_key, turbot_api_secret_key) = turbotutils.get_turbot_access_keys()
+
+    get_account_tags(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host,
+                     turbot_account)
