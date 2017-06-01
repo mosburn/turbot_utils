12f8cd369d8dc6ba2572e98dfa3a4096ed9670ed
Michael Osburn <michael.osburn@mheducation.com>
2017-06-01 12:15:52 -0700

    adding script for user ssh key management

--- examples/delete_ssh_keys_for_user.py
+++ examples/delete_ssh_keys_for_user.py
@@ -6,12 +6,28 @@ import urllib.parse
 import json
 
 
-def create_user_ssh_keys(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host, turbot_user_id):
+def delete_fingerprint(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host, turbot_user_id, finger):
+    api_method = "DELETE"
+    api_url = "/api/v1/users/%s/sshKeys/%s" % (turbot_user_id, finger)
+    response = requests.request(
+        api_method,
+        urllib.parse.urljoin(turbot_host, api_url),
+        auth=(turbot_api_access_key, turbot_api_secret_key),
+        verify=turbot_host_certificate_verification,
+        headers={
+            'content-type': "application/json",
+            'cache-control': "no-cache"
+        }
+    )
+
+
+def get_ssh_fingerprints(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host, turbot_user_id):
     """ Sets user access AKIA key pairs for a specified account
 
     NOTE: This requires a Cluster role Turbot/Owner or higher in order to work.
     """
-    api_method = "POST"
+    fingerprints = []
+    api_method = "GET"
     api_url = "/api/v1/users/%s/sshKeys" % (turbot_user_id)
     response = requests.request(
         api_method,
@@ -25,7 +41,10 @@ def create_user_ssh_keys(turbot_api_access_key, turbot_api_secret_key, turbot_ho
     )
 
     responseObj = json.loads(response.text)
-    return(responseObj['privateKey'])
+
+    for item in responseObj['items']:
+        fingerprints.append(item['fingerprint'])
+    return(fingerprints)
 
 
 if __name__ == '__main__':
@@ -37,10 +56,12 @@ if __name__ == '__main__':
     turbot_user_id = turbotutils.get_turbot_user()
     # Get the access and secret key pairs
     (turbot_api_access_key,turbot_api_secret_key) = turbotutils.get_turbot_access_keys()
-
+    delete = 'YES'
     turbot_user_id= 'dummy_user'
     cluster_id = turbotutils.cluster.get_cluster_id(turbot_host, turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification)
-    new_key = create_user_ssh_keys(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host, turbot_user_id)
+    fingerprints = get_ssh_fingerprints(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host, turbot_user_id)
 
-    with open(turbot_user_id, 'w') as f:
-        f.write(new_key)
+    if not fingerprints:
+        if delete == 'YES':
+            for finger in fingerprints:
+                delete_fingerprint(turbot_api_access_key, turbot_api_secret_key, turbot_host_certificate_verification, turbot_host, turbot_user_id, finger)
